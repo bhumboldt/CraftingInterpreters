cbc0f5263d06cba09896d038962986fc9325e0cd
Brandon Humboldt <bhumboldt@wisc.edu>
2020-04-26 10:25:36 -0500

    Chapter 4, Scanning

---
+++ CraftingInterpreters/src/app/Lox.java
@@ -0,0 +1,62 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+
+        System.out.println("Hello Lox!");
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader inputReader = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(inputReader);
+
+        while (true) {
+            System.out.println("> ");
+            run(reader.readLine());
+            hadError = false;
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError)
+            System.exit(65);
+    }
+
+    private static void run(String src) {
+        Scanner scanner = new Scanner(src);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+}
